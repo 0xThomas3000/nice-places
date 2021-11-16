a0023366cb53379db65f437697e649d9c833d584
tuanit168 <tuanit168@gmail.com>
2021-11-16 23:31:32 +1100

    Edit the initial data model declaration

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import '../models/place.dart';
 
 class GreatPlaces with ChangeNotifier {
-  final List<Place> _items = [];
+  List<Place> _items = [];
 
   List<Place> get items {
     return [..._items];
