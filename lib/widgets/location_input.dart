87b5588bfcad307382133ed71f33f81d8b21493c
tuanit168 <tuanit168@gmail.com>
2021-11-25 22:22:19 +1100

    Fetching the user coordinates and print it out on the screen

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   @override
@@ -7,6 +8,11 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   String? _previewImageUrl;
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+    print(locData.latitude);
+    print(locData.longitude);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +51,7 @@ class _LocationInputState extends State<LocationInput> {
                 foregroundColor: MaterialStateProperty.all(
                     Theme.of(context).colorScheme.primary),
               ),
-              onPressed: () {},
+              onPressed: _getCurrentUserLocation,
             ),
             TextButton.icon(
               icon: const Icon(
